c6acb54d635df17eacc638ea9e28ea68e6403b7a
Brett V. Forsgren <brettfo@microsoft.com>
2019-05-22 14:28:22 -0700

    run npm tests when appropriate

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0eng\common\build.ps1" -build -binaryLog %*
+powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0eng\build.ps1" -build -binaryLog %*
