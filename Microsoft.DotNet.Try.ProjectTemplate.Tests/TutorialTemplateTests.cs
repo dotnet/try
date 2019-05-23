bea2306888c78097066a70d000f1cc84a41a42bd
Akshita <akagarw@microsoft.com>
2019-05-23 14:11:44 -0700

    add the copyright header and add the project to the solution

--- Microsoft.DotNet.Try.ProjectTemplate.Tests/TutorialTemplateTests.cs
+++ Microsoft.DotNet.Try.ProjectTemplate.Tests/TutorialTemplateTests.cs
@@ -1,4 +1,7 @@
-﻿using WorkspaceServer;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using WorkspaceServer;
 using Xunit;
 using WorkspaceServer.Tests;
 using System.IO;
