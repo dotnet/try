8b127cca0c0c2bd3a7647af0c544ec22c4e50966
Diego <dicolomb@microsoft.com>
2019-05-24 00:46:12 +0100

    using pipeline in project asset

--- WorkspaceServer/Packaging/ProjectAsset.cs
+++ WorkspaceServer/Packaging/ProjectAsset.cs
@@ -2,14 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Concurrent;
 using System.IO;
 using System.Linq;
-using System.Reactive.Concurrency;
-using System.Reactive.Disposables;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Threading;
+using System.Reactive;
 using System.Threading.Tasks;
 using Buildalyzer;
 using Clockwise;
@@ -19,7 +14,7 @@ using MLS.Agent.Tools;
 using Pocket;
 using WorkspaceServer.Servers.Roslyn;
 using static Pocket.Logger<WorkspaceServer.Packaging.ProjectAsset>;
-using Disposable = System.Reactive.Disposables.Disposable;
+
 
 namespace WorkspaceServer.Packaging
 {
@@ -30,20 +25,11 @@ namespace WorkspaceServer.Packaging
     {
         private readonly IDirectoryAccessor _directoryAccessor;
         private const string FullBuildBinlogFileName = "package_fullBuild.binlog";
-
-        private Workspace _workspace;
         private readonly FileInfo _projectFile;
-        private readonly SemaphoreSlim _buildSemaphore;
-        private readonly object _fullBuildCompletionSourceLock = new object();
-        private TaskCompletionSource<Workspace> _fullBuildCompletionSource = new TaskCompletionSource<Workspace>();
-        private readonly Subject<Budget> _fullBuildRequestChannel;
-
-        private readonly IScheduler _buildThrottleScheduler;
-        private readonly SerialDisposable _fullBuildThrottlerSubscription;
-       
         private readonly FileInfo _lastBuildErrorLogFile;
-
-        private static readonly ConcurrentDictionary<string, SemaphoreSlim> PackageBuildSemaphores = new ConcurrentDictionary<string, SemaphoreSlim>();
+        private PipelineStep<AnalyzerResult> _projectBuildStep;
+        private PipelineStep<Workspace> _workspaceStep;
+        private PipelineStep<Unit> _cleanupStep;
 
         public string Name { get; }
 
@@ -72,145 +58,54 @@ namespace WorkspaceServer.Packaging
 
             Name = _projectFile.Name;
             Directory = DirectoryAccessor.GetFullyQualifiedPath(new RelativeDirectoryPath(".")) as DirectoryInfo;
-
-            _buildThrottleScheduler =  TaskPoolScheduler.Default;
-            _fullBuildRequestChannel = new Subject<Budget>();
-            _fullBuildThrottlerSubscription = new SerialDisposable();
-            _buildSemaphore = PackageBuildSemaphores.GetOrAdd(Name, _ => new SemaphoreSlim(1, 1));
             _lastBuildErrorLogFile = directoryAccessor.GetFullyQualifiedPath(new RelativeFilePath("./.trydotnet-builderror")) as FileInfo;
-            SetupWorkspaceCreationFromBuildChannel();
+
+            _cleanupStep = new PipelineStep<Unit>(CleanDirectoryAsync);
+            _projectBuildStep = _cleanupStep.Then((_)=> BuildProjectAsync());
+            _workspaceStep = _projectBuildStep.Then(BuildWorkspaceAsync);
         }
-        public Task<Workspace> CreateRoslynWorkspaceAsync(Budget budget)
+
+        private Task<Unit> CleanDirectoryAsync()
         {
-            if (_workspace != null)
+            var to_clean = Directory.GetDirectories("obj");
+            foreach (var directoryInfo in to_clean)
             {
-                return Task.FromResult(_workspace);
+                directoryInfo.Delete(true);
             }
 
-            CreateCompletionSourceIfNeeded(ref _fullBuildCompletionSource, _fullBuildCompletionSourceLock);
-
-            _fullBuildRequestChannel.OnNext(budget);
-
-            return _fullBuildCompletionSource.Task;
+            return Task.FromResult(Unit.Default);
         }
 
-        public Task<Workspace> CreateRoslynWorkspaceForRunAsync(Budget budget)
+        private Task<Workspace> BuildWorkspaceAsync(AnalyzerResult result)
         {
-            return CreateRoslynWorkspaceAsync(budget);
-        }
-
-        public Task<Workspace> CreateRoslynWorkspaceForLanguageServicesAsync(Budget budget)
-        {
-            return CreateRoslynWorkspaceAsync(budget);
-        }
-
-        private void SetupWorkspaceCreationFromBuildChannel()
-        {
-            _fullBuildThrottlerSubscription.Disposable = _fullBuildRequestChannel
-                .Throttle(TimeSpan.FromSeconds(0.5), _buildThrottleScheduler)
-                .ObserveOn(TaskPoolScheduler.Default)
-                .Subscribe(
-                    async (budget) =>
-                    {
-                        try
-                        {
-                            await ProcessFullBuildRequest(budget);
-                        }
-                        catch (Exception e)
-                        {
-                            SetCompletionSourceException(_fullBuildCompletionSource, e, _fullBuildCompletionSourceLock);
-                        }
-                    },
-                    error =>
-                    {
-                        SetCompletionSourceException(_fullBuildCompletionSource, error, _fullBuildCompletionSourceLock);
-                        SetupWorkspaceCreationFromBuildChannel();
-                    });
-        }
-
-        private void SetCompletionSourceResult(TaskCompletionSource<Workspace> completionSource, Workspace result, object lockObject)
-        {
-            lock (lockObject)
+            if (result.TryGetWorkspace(out var ws))
             {
-                switch (completionSource.Task.Status)
-                {
-                    case TaskStatus.Canceled:
-                    case TaskStatus.Faulted:
-                    case TaskStatus.RanToCompletion:
-                        return;
-                    default:
-                        completionSource.SetResult(result);
-                        break;
-                }
+                var projectId = ws.CurrentSolution.ProjectIds.FirstOrDefault();
+                var references = result.References;
+                var metadataReferences = references.GetMetadataReferences();
+                var solution = ws.CurrentSolution;
+                solution = solution.WithProjectMetadataReferences(projectId, metadataReferences);
+                ws.TryApplyChanges(solution);
+                return Task.FromResult(ws);
             }
-        }
-        private void CreateCompletionSourceIfNeeded(ref TaskCompletionSource<Workspace> completionSource, object lockObject)
-        {
-            lock (lockObject)
-            {
-                switch (completionSource.Task.Status)
-                {
-                    case TaskStatus.Canceled:
-                    case TaskStatus.Faulted:
-                    case TaskStatus.RanToCompletion:
-                        completionSource = new TaskCompletionSource<Workspace>();
-                        break;
-                }
-            }
+            throw new InvalidOperationException("Failed creating workspace");
         }
 
-        private void SetCompletionSourceException(TaskCompletionSource<Workspace> completionSource, Exception exception, object lockObject)
-        {
-            lock (lockObject)
-            {
-                switch (completionSource.Task.Status)
-                {
-                    case TaskStatus.Canceled:
-                    case TaskStatus.Faulted:
-                    case TaskStatus.RanToCompletion:
-                        return;
-                    default:
-                        completionSource.SetException(exception);
-                        break;
-                }
-            }
-        }
-
-        private async Task ProcessFullBuildRequest(Budget budget)
-        {
-            await FullBuild().CancelIfExceeds(budget);
-            SetCompletionSourceResult(_fullBuildCompletionSource, _workspace, _fullBuildCompletionSourceLock);
-        }
-
-        public async Task FullBuild()
+        private async Task<AnalyzerResult> BuildProjectAsync()
         {
             using (var operation = Log.OnEnterAndConfirmOnExit())
             {
                 try
                 {
                     operation.Info("Attempting building package {name}", Name);
-
-                    var buildInProgress = _buildSemaphore.CurrentCount == 0;
-                    await _buildSemaphore.WaitAsync();
-
-                    using (Disposable.Create(() => _buildSemaphore.Release()))
-                    {
-                        if (buildInProgress)
-                        {
-                            operation.Info("Skipping build for package {name}", Name);
-                            return;
-                        }
-
-                        await DotnetBuild();
-                    }
-
+                    await DotnetBuild();
                     operation.Info("Workspace built");
-
                     operation.Succeed();
                 }
                 catch (Exception exception)
                 {
                     operation.Error("Exception building workspace", exception);
+                    throw;
                 }
 
                 var binLog = this.FindLatestBinLog();
@@ -228,27 +123,35 @@ namespace WorkspaceServer.Packaging
                 {
                     if (result.Succeeded)
                     {
-                        if (result.TryGetWorkspace(out var ws))
-                        {
-                            var projectId = ws.CurrentSolution.ProjectIds.FirstOrDefault();
-                            var references = result.References;
-                            var metadataReferences = references.GetMetadataReferences();
-                            var solution = ws.CurrentSolution;
-                            solution = solution.WithProjectMetadataReferences(projectId, metadataReferences);
-                            ws.TryApplyChanges(solution);
-                            _workspace = ws;
-                        }
+                        return result;
                     }
+                    throw new InvalidOperationException("Failed to build");
                 }
+
+                throw new InvalidOperationException("Failed to build");
             }
         }
+
+        public Task<Workspace> CreateRoslynWorkspaceAsync(Budget budget)
+        {
+            return _workspaceStep.GetLatestAsync();
+        }
+
+        public Task<Workspace> CreateRoslynWorkspaceForRunAsync(Budget budget)
+        {
+            return CreateRoslynWorkspaceAsync(budget);
+        }
+
+        public Task<Workspace> CreateRoslynWorkspaceForLanguageServicesAsync(Budget budget)
+        {
+            return CreateRoslynWorkspaceAsync(budget);
+        }
+
 
         protected async Task DotnetBuild()
         {
             using (var operation = Log.OnEnterAndConfirmOnExit())
             {
-                
-                   
                 var args = $"/bl:{FullBuildBinlogFileName}";
                 if (_projectFile?.Exists == true)
                 {
@@ -256,8 +159,7 @@ namespace WorkspaceServer.Packaging
                 }
 
                 operation.Info("Building package {name} in {directory}", Name, Directory);
-
-
+                
                 var result = await new Dotnet(Directory).Build(args: args);
 
                 if (result.ExitCode != 0)
@@ -275,8 +177,5 @@ namespace WorkspaceServer.Packaging
                 operation.Succeed();
             }
         }
-
-
-      
     }
 }
