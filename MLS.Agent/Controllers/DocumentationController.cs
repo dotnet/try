d0af44618118ee05d48322052a19ab8eef6ba0b8
Diego <dicolomb@microsoft.com>
2019-05-30 14:24:20 +0100

    separate math support code

--- MLS.Agent/Controllers/DocumentationController.cs
+++ MLS.Agent/Controllers/DocumentationController.cs
@@ -120,12 +120,7 @@ namespace MLS.Agent.Controllers
     <script src=""/api/trydotnet.min.js?v={_cacheBuster}""></script>
     <link rel=""stylesheet"" href=""/css/trydotnet.css?v={_cacheBuster}"">  
     <link rel=""icon"" type=""image/png"" href=""favicon-32x32.png"">
-    <script type=""text/x-mathjax-config"">
-    MathJax.Hub.Config({{
-        tex2jax: {{inlineMath: [[""$"",""$""],[""\\("",""\\)""]]}}
-    }});
-    </script>
-    <script type=""text/javascript"" src=""/mathjax/MathJax.js?config=TeX-AMS_HTML-full""></script>
+    {MathSupport()}
     <title>dotnet try - {markdownFile.Path.Value.HtmlEncode()}</title>
 </head>
 
@@ -143,6 +138,17 @@ namespace MLS.Agent.Controllers
 </body>
 
 </html>".ToHtmlContent();
+
+        private IHtmlContent MathSupport() =>
+            @"
+    <script type=""text/x-mathjax-config"">
+        MathJax.Hub.Config({
+        tex2jax: {inlineMath: [[""$"",""$""],[""\\("",""\\)""]]},       
+        showMathMenu: false,
+        showMathMenuMSIE: false
+    });
+    </script>
+    <script type=""text/javascript"" src=""/mathjax/MathJax.js?config=TeX-AMS_SVG""></script>".ToHtmlContent();
 
         private async Task<IHtmlContent> OneColumnLayoutScaffold(string hostUrl, MarkdownFile markdownFile) =>
             Layout(hostUrl, markdownFile,
