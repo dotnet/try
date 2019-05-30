c13f6ea9b6bf23132c78916b90c9133a68a0d252
Diego <dicolomb@microsoft.com>
2019-05-30 11:27:46 +0100

    add write elements

--- eng/build.ps1
+++ eng/build.ps1
@@ -3,8 +3,11 @@ $ErrorActionPreference = "Stop"
 
 function TestUsingNPM([string] $testPath) {
     Push-Location $testPath
+    Write-Host "Installing packages"
     Start-Process -PassThru -WindowStyle Hidden -Wait npm "i"
+    Write-Host "Testing"
     $test = Start-Process -PassThru -WindowStyle Hidden -Wait npm "run ciTest"
+    Write-Host "Done with code "+$test.ExitCode
     Pop-Location
     return $test.ExitCode
 }
