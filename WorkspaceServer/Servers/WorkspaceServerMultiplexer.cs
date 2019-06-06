5b4b8ea4f94fe6b59d510c18e8ccb32fc7592124
Diego <dicolomb@microsoft.com>
2019-06-06 23:41:36 +0100

    use language switch

--- WorkspaceServer/Servers/WorkspaceServerMultiplexer.cs
+++ WorkspaceServer/Servers/WorkspaceServerMultiplexer.cs
@@ -11,55 +11,54 @@ namespace WorkspaceServer.Servers
     public class WorkspaceServerMultiplexer : IWorkspaceServer
     {
         private IPackageFinder _packageFinder;
-        private IWorkspaceServer _roslynWorkspaceServer;
-        private IWorkspaceServer _fsharpWorksapceServer;
+        private readonly IWorkspaceServer _roslynWorkspaceServer;
+        private readonly IWorkspaceServer _fsharpWorkspaceServer;
 
         public WorkspaceServerMultiplexer(IPackageFinder packageFinder)
         {
             _packageFinder = packageFinder;
             _roslynWorkspaceServer = new RoslynWorkspaceServer(packageFinder);
-            _fsharpWorksapceServer = new FSharpWorkspaceServer(packageFinder);
+            _fsharpWorkspaceServer = new FSharpWorkspaceServer(packageFinder);
         }
 
         public async Task<CompileResult> Compile(WorkspaceRequest request, Budget budget = null)
         {
-            return await IsFSharpWorkspaceRequest(request.Workspace)
-                ? await _fsharpWorksapceServer.Compile(request, budget)
+            return IsFSharpWorkspaceRequest(request.Workspace)
+                ? await _fsharpWorkspaceServer.Compile(request, budget)
                 : await _roslynWorkspaceServer.Compile(request, budget);
         }
 
         public async Task<CompletionResult> GetCompletionList(WorkspaceRequest request, Budget budget = null)
         {
-            return await IsFSharpWorkspaceRequest(request.Workspace)
-                ? await _fsharpWorksapceServer.GetCompletionList(request, budget)
+            return IsFSharpWorkspaceRequest(request.Workspace)
+                ? await _fsharpWorkspaceServer.GetCompletionList(request, budget)
                 : await _roslynWorkspaceServer.GetCompletionList(request, budget);
         }
 
         public async Task<DiagnosticResult> GetDiagnostics(WorkspaceRequest request, Budget budget = null)
         {
-            return await IsFSharpWorkspaceRequest(request.Workspace)
-                ? await _fsharpWorksapceServer.GetDiagnostics(request, budget)
+            return IsFSharpWorkspaceRequest(request.Workspace)
+                ? await _fsharpWorkspaceServer.GetDiagnostics(request, budget)
                 : await _roslynWorkspaceServer.GetDiagnostics(request, budget);
         }
 
         public async Task<SignatureHelpResult> GetSignatureHelp(WorkspaceRequest request, Budget budget = null)
         {
-            return await IsFSharpWorkspaceRequest(request.Workspace)
-                ? await _fsharpWorksapceServer.GetSignatureHelp(request, budget)
+            return IsFSharpWorkspaceRequest(request.Workspace)
+                ? await _fsharpWorkspaceServer.GetSignatureHelp(request, budget)
                 : await _roslynWorkspaceServer.GetSignatureHelp(request, budget);
         }
 
         public async Task<RunResult> Run(WorkspaceRequest request, Budget budget = null)
         {
-            return await IsFSharpWorkspaceRequest(request.Workspace)
-                ? await _fsharpWorksapceServer.Run(request, budget)
+            return IsFSharpWorkspaceRequest(request.Workspace)
+                ? await _fsharpWorkspaceServer.Run(request, budget)
                 : await _roslynWorkspaceServer.Run(request, budget);
         }
 
-        private async Task<bool> IsFSharpWorkspaceRequest(Workspace workspace)
+        private bool IsFSharpWorkspaceRequest(Workspace workspace)
         {
-            var package = await _packageFinder.Find<Package>(workspace.WorkspaceType);
-            return package.Initializer.Language == "F#";
+            return workspace.Language == "fsharp";
         }
     }
 }
