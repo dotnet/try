7f5ea14ad556577fbefd747e1a928434f21cb119
Diego <dicolomb@microsoft.com>
2019-06-10 17:49:14 +0100

    fix problem with language Info in kernel reply

--- Microsoft.DotNet.Try.Jupyter/JupyterRequestContextHandler.cs
+++ Microsoft.DotNet.Try.Jupyter/JupyterRequestContextHandler.cs
@@ -34,135 +34,138 @@ namespace Microsoft.DotNet.Try.Jupyter
             switch (delivery.Command.Request.Header.MessageType)
             {
                 case MessageTypeValues.ExecuteRequest:
-                    var transient = new Dictionary<string, object> { { "display_id", Guid.NewGuid().ToString() } };
-
-                    var jObject = (JObject)delivery.Command.Request.Content;
-                    var executeRequest = jObject.ToObject<ExecuteRequest>();
-
-                    var code = executeRequest.Code;
-
-                    var workspace = CreateScaffoldWorkspace(code);
-
-                    var workspaceRequest = new WorkspaceRequest(workspace);
-
-                    var result = await _server.Run(workspaceRequest);
-
-                    var messageBuilder = delivery.Command.Builder;
-                    var ioPubChannel = delivery.Command.IoPubChannel;
-                    var serverChannel = delivery.Command.ServerChannel;
-
-                    if (!executeRequest.Silent)
-                    {
-                        _executionCount++;
-
-                        var executeInput = messageBuilder.CreateMessage(
-                            new ExecuteInput
-                            {
-                                Code = code,
-                                ExecutionCount = _executionCount
-                            },
-                            delivery.Command.Request.Header);
-
-                        ioPubChannel.Send(executeInput);
-                    }
-
-                    // execute result
-                    var output = string.Join("\n", result.Output);
-
-                    
-                    // executeResult data
-                    var executeResultData = new ExecuteResult
-                    {
-                        Data = new JObject
-                        {
-                            { "text/html", output },
-                            { "text/plain", output }
-                        },
-                        Transient = transient,
-                        ExecutionCount = _executionCount
-                    };
-
-
-
-                    var resultSucceeded = result.Succeeded &&
-                                          result.Exception == null;
-
-                    if (resultSucceeded)
-                    {
-                        // reply ok
-                        var executeReplyPayload = new ExecuteReplyOk
-                        {
-                            ExecutionCount = _executionCount
-                        };
-
-                        // send to server
-                        var executeReply = messageBuilder.CreateMessage(
-                            executeReplyPayload, 
-                            delivery.Command.Request.Header);
-
-                        executeReply.Identifiers = delivery.Command.Request.Identifiers;
-
-                        serverChannel.Send(executeReply);
-                    }
-                    else
-                    {
-                        var errorContent = new Error
-                        {
-                            EName = string.IsNullOrWhiteSpace(result.Exception)
-                                        ? "Compile Error" 
-                                        : "Unhandled Exception",
-                            EValue = output,
-                            Traceback = new List<string>()
-                        };
-
-                        //  reply Error
-                        var executeReplyPayload = new ExecuteReplyError(errorContent)
-                        {
-                            ExecutionCount = _executionCount
-                        };
-
-                        // send to server
-                        var executeReply = messageBuilder.CreateMessage(
-                            executeReplyPayload, 
-                            delivery.Command.Request.Header);
-
-                        executeReply.Identifiers = delivery.Command.Request.Identifiers;
-
-                        serverChannel.Send(executeReply);
-
-                        if (!executeRequest.Silent)
-                        {
-                            // send on io
-                            var error = messageBuilder.CreateMessage(
-                                errorContent, 
-                                delivery.Command.Request.Header);
-                            ioPubChannel.Send(error);
-
-                            // send on stderr
-                            var stdErr = new StdErrStream
-                            {
-                                Text = errorContent.EValue
-                            };
-                            var stream = messageBuilder.CreateMessage(
-                                stdErr, 
-                                delivery.Command.Request.Header);
-                            ioPubChannel.Send(stream);
-                        }
-                    }
-
-                    if (!executeRequest.Silent && resultSucceeded)
-                    {
-                        // send on io
-                        var executeResultMessage = messageBuilder.CreateMessage(
-                            executeResultData,
-                            delivery.Command.Request.Header);
-                        ioPubChannel.Send(executeResultMessage);
-                    }
-
+                    await HandleExecuteRequest(delivery);
                     break;
             }
 
             return delivery.Complete();
+        }
+
+        private async Task HandleExecuteRequest(ICommandDelivery<JupyterRequestContext> delivery)
+        {
+            var transient = new Dictionary<string, object> {{"display_id", Guid.NewGuid().ToString()}};
+
+            var jObject = (JObject) delivery.Command.Request.Content;
+            var executeRequest = jObject.ToObject<ExecuteRequest>();
+
+            var code = executeRequest.Code;
+
+            var workspace = CreateScaffoldWorkspace(code);
+
+            var workspaceRequest = new WorkspaceRequest(workspace);
+
+            var result = await _server.Run(workspaceRequest);
+
+            var messageBuilder = delivery.Command.Builder;
+            var ioPubChannel = delivery.Command.IoPubChannel;
+            var serverChannel = delivery.Command.ServerChannel;
+
+            if (!executeRequest.Silent)
+            {
+                _executionCount++;
+
+                var executeInput = messageBuilder.CreateMessage(
+                    new ExecuteInput
+                    {
+                        Code = code,
+                        ExecutionCount = _executionCount
+                    },
+                    delivery.Command.Request.Header);
+
+                ioPubChannel.Send(executeInput);
+            }
+
+            // execute result
+            var output = string.Join("\n", result.Output);
+
+
+            // executeResult data
+            var executeResultData = new ExecuteResult
+            {
+                Data = new JObject
+                {
+                    {"text/html", output},
+                    {"text/plain", output}
+                },
+                Transient = transient,
+                ExecutionCount = _executionCount
+            };
+
+
+            var resultSucceeded = result.Succeeded &&
+                                  result.Exception == null;
+
+            if (resultSucceeded)
+            {
+                // reply ok
+                var executeReplyPayload = new ExecuteReplyOk
+                {
+                    ExecutionCount = _executionCount
+                };
+
+                // send to server
+                var executeReply = messageBuilder.CreateMessage(
+                    executeReplyPayload,
+                    delivery.Command.Request.Header);
+
+                executeReply.Identifiers = delivery.Command.Request.Identifiers;
+
+                serverChannel.Send(executeReply);
+            }
+            else
+            {
+                var errorContent = new Error
+                {
+                    EName = string.IsNullOrWhiteSpace(result.Exception)
+                        ? "Compile Error"
+                        : "Unhandled Exception",
+                    EValue = output,
+                    Traceback = new List<string>()
+                };
+
+                //  reply Error
+                var executeReplyPayload = new ExecuteReplyError(errorContent)
+                {
+                    ExecutionCount = _executionCount
+                };
+
+                // send to server
+                var executeReply = messageBuilder.CreateMessage(
+                    executeReplyPayload,
+                    delivery.Command.Request.Header);
+
+                executeReply.Identifiers = delivery.Command.Request.Identifiers;
+
+                serverChannel.Send(executeReply);
+
+                if (!executeRequest.Silent)
+                {
+                    // send on io
+                    var error = messageBuilder.CreateMessage(
+                        errorContent,
+                        delivery.Command.Request.Header);
+                    ioPubChannel.Send(error);
+
+                    // send on stderr
+                    var stdErr = new StdErrStream
+                    {
+                        Text = errorContent.EValue
+                    };
+                    var stream = messageBuilder.CreateMessage(
+                        stdErr,
+                        delivery.Command.Request.Header);
+                    ioPubChannel.Send(stream);
+                }
+            }
+
+            if (!executeRequest.Silent && resultSucceeded)
+            {
+                // send on io
+                var executeResultMessage = messageBuilder.CreateMessage(
+                    executeResultData,
+                    delivery.Command.Request.Header);
+                ioPubChannel.Send(executeResultMessage);
+            }
         }
 
         private static Workspace CreateScaffoldWorkspace(string code)
