e940490186830343930bea5580bde3d3b61008b2
Ravi Chande <rchande@microsoft.com>
2019-07-01 15:11:26 -0700

    Unregressed wasm code runner

--- MLS.Blazor/wwwroot/interop.js
+++ MLS.Blazor/wwwroot/interop.js
@@ -18,19 +18,14 @@ window.BlazorInterop = {
 };
 
 
-var seq = -1;
 window.addEventListener("message", e => {
-    console.log("Interop js received: ", e);
-    console.log(e.data);
 
-    if (e.data.Sequence !== undefined && e.data.Sequence > seq) {
-        seq = e.data.Sequence;
         dotnetInstance.invokeMethodAsync("MLS.Blazor.PostMessageAsync", e.data)
             .then(r => {
-                console.log(r);
-                window.postMessage(r, "*");
+                if (r !== null) {
+                    window.postMessage(r, "*");
+                }
             });
-    }
 
     
 });
