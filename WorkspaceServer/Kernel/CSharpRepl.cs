6d188b9b799d01344fca0a07e966d2932d3cb872
Diego <dicolomb@microsoft.com>
2019-07-19 07:59:26 +0100

    add header to integration tests

--- WorkspaceServer/Kernel/CSharpRepl.cs
+++ WorkspaceServer/Kernel/CSharpRepl.cs
@@ -16,7 +16,6 @@ using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Recommendations;
 using Microsoft.CodeAnalysis.Scripting;
 using WorkspaceServer.LanguageServices;
-using CompletionItem = Microsoft.DotNet.Try.Protocol.CompletionItem;
 using Microsoft.DotNet.Interactive.Rendering;
 using WorkspaceServer.Servers.Scripting;
 using Task = System.Threading.Tasks.Task;
@@ -239,7 +238,7 @@ namespace WorkspaceServer.Kernel
                     symbolToSymbolKey[key] = symbol;
                 }
             }
-            var items = completionList.Items.Select(item => item.ToModel(symbolToSymbolKey, document)).ToArray();
+            var items = completionList.Items.Select(item => item.ToModel(symbolToSymbolKey, document).ToDomainObject()).ToArray();
 
             return items;
         }
---
+++ WorkspaceServer/Kernel/ProtocolObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+namespace WorkspaceServer.Kernel
+{
+    internal static class ProtocolObjectExtensions{
+        public static CompletionItem ToDomainObject(this Microsoft.DotNet.Try.Protocol.CompletionItem source)
+        {
+            return new CompletionItem(source.DisplayText, source.Kind, source.FilterText, source.SortText, source.InsertText, source.Documentation?.Value);
+        }
+    }
+}
