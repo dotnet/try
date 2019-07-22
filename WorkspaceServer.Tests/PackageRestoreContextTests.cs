88f8e3470bc834065ae55536c418256564d849c3
Ravi Chande <rchande@microsoft.com>
2019-07-22 11:37:40 -0700

    Make a test pass

---
+++ WorkspaceServer.Tests/PackageRestoreContextTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using WorkspaceServer.PackageRestore;
+using Xunit;
+
+namespace WorkspaceServer.Tests
+{
+    public class PackageRestoreContextTests
+    {
+        [Fact]
+        public async Task It_works()
+        {
+            using (var context = new PackageRestoreContext())
+            {
+                var refs = await context.AddPackage("FluentAssertions", "5.7.0");
+                refs.ToArray().Length.Should().Be(2);
+            }
+        }
+    }
+}
--- WorkspaceServer/PackageRestore/PackageRestoreContext.cs
+++ WorkspaceServer/PackageRestore/PackageRestoreContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Clockwise;
@@ -36,7 +38,8 @@ namespace WorkspaceServer.PackageRestore
 
         private async Task<Package> CreatePackage()
         {
-            var packageBuilder = new PackageBuilder(Guid.NewGuid().ToString("N8"));
+            var packageBuilder = new PackageBuilder(Guid.NewGuid().ToString("N"));
+            packageBuilder.CreateRebuildablePackage = true;
             packageBuilder.CreateUsingDotnet("console");
             packageBuilder.TrySetLanguageVersion("8.0");
             packageBuilder.AddPackageReference("Newtonsoft.Json");
@@ -45,14 +48,28 @@ namespace WorkspaceServer.PackageRestore
             return package;
         }
 
-        public async Task<IEnumerable<MetadataReference>> AddPackage(string package, string version)
+        public async Task<IEnumerable<MetadataReference>> AddPackage(string packageName, string packageVersion)
         {
+            var package = await _lazyPackage.ValueAsync();
+            var currentWorkspace = await package.CreateRoslynWorkspaceForRunAsync(new Budget());
+            var currentRefs = new HashSet<string>(
+                currentWorkspace.CurrentSolution.Projects.First().MetadataReferences
+                .Select(m => m.Display));
+
+            var dotnet = new Dotnet(package.Directory);
+            var result = await dotnet.AddPackage(packageName, packageVersion);
+
+            var newWorkspace = await package.CreateRoslynWorkspaceForRunAsync(new Budget());
+            var newRefs = new HashSet<MetadataReference>(newWorkspace.CurrentSolution.Projects.First().MetadataReferences);
+
+            var resultRefs = newRefs.Where(n => !currentRefs.Contains(n.Display));
+            return resultRefs;
 
         }
 
         public IEnumerable<MetadataReference> AccumulatedReferences()
         {
-
+            return null;
         }
 
         public void Dispose()
