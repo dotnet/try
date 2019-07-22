3db37c1a458f0347432470c2962cb9e4bce6e5ba
Ravi Chande <rchande@microsoft.com>
2019-07-22 10:09:55 -0700

    Condition integration tests on env var RUN_DOTNET_TRY_INTEGRATION_TESTS

---
+++ NotIntegrationTests/ConditionalFact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace NotIntegrationTests
+{
+    public class ConditionalFact : FactAttribute
+    {
+        const string RUN_DOTNET_TRY_INTEGRATION_TESTS = nameof(RUN_DOTNET_TRY_INTEGRATION_TESTS);
+
+        public ConditionalFact()
+        {
+            var variable = Environment.GetEnvironmentVariable(RUN_DOTNET_TRY_INTEGRATION_TESTS);
+            if (bool.TryParse(variable, out var value) && value)
+            {
+                base.Skip = null;
+            }
+            else
+            {
+                base.Skip = base.Skip ?? $"Integration tests disabled. Set {RUN_DOTNET_TRY_INTEGRATION_TESTS}=true to run them";
+            }
+        }
+    }
+}
