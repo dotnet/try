dc2101c58f9d5208c15356df3eba4222704eaa07
Akshita <akagarw@microsoft.com>
2019-08-02 10:33:55 -0700

    add --user

--- MLS.Agent/FileSystemJupyterKernelSpec.cs
+++ MLS.Agent/FileSystemJupyterKernelSpec.cs
@@ -24,7 +24,7 @@ namespace MLS.Agent
 
         public Task<CommandLineResult> InstallKernel(DirectoryInfo sourceDirectory)
         {
-            return ExecuteCommand($"install {sourceDirectory.FullName}");
+            return ExecuteCommand($"install {sourceDirectory.FullName}", "--user");
         }
 
         public static bool CheckIfJupyterKernelSpecExists()
