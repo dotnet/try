b3b7e93cb2424a22c2045247610b640659a9ed67
Akshita <akagarw@microsoft.com>
2019-08-02 10:06:39 -0700

    use which command to execute

--- MLS.Agent/FileSystemJupyterKernelSpec.cs
+++ MLS.Agent/FileSystemJupyterKernelSpec.cs
@@ -2,8 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using MLS.Agent.Tools;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace MLS.Agent
@@ -27,7 +29,8 @@ namespace MLS.Agent
 
         public static async Task<bool> CheckIfJupyterKernelSpecExists()
         {
-            var checkJupyterInstall = await Tools.CommandLine.Execute("where", "jupyter-kernelspec");
+            var command = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "where" : "which";
+            var checkJupyterInstall = await Tools.CommandLine.Execute(command, "jupyter-kernelspec");
             return checkJupyterInstall.ExitCode == 0;
         }
     }
