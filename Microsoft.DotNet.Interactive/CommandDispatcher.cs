ea44cb8474ef30e18a7076cd06d7cef2d0ac5a88
Ravi Chande <rchande@microsoft.com>
2019-08-12 09:45:14 -0700

    Don't hardcode type conversion (#368)
    Dynamically deserialize command messages

---
+++ Microsoft.DotNet.Interactive/CommandDispatcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.DotNet.Interactive.Commands;
+using Newtonsoft.Json;
+
+namespace Microsoft.DotNet.Interactive
+{
+    class CommandDispatcher
+    {
+        private readonly Dictionary<string, Type> _map;
+
+        public CommandDispatcher()
+        {
+            _map = Assembly
+               .GetExecutingAssembly()
+               .GetTypes()
+               .Where(t => typeof(IKernelCommand).IsAssignableFrom(t)
+                        && !t.IsAbstract
+                        && !t.IsGenericTypeDefinition
+                        && !t.IsInterface)
+               .ToDictionary(t => t.Name, t => t);
+        }
+
+        public IKernelCommand Dispatch(string commandType, string body)
+        {
+            if (!_map.ContainsKey(commandType))
+            {
+                return null;
+            }
+
+            return (IKernelCommand)JsonConvert.DeserializeObject(body, _map[commandType]);
+        }
+    }
+}
