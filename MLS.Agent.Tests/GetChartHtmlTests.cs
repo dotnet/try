c1514c0816d8b92073d627718373865fd6e202a0
Akshita <akagarw@microsoft.com>
2019-08-19 16:24:00 -0700

    Use a specific version of plotly and a context

--- MLS.Agent.Tests/GetChartHtmlTests.cs
+++ MLS.Agent.Tests/GetChartHtmlTests.cs
@@ -34,7 +34,7 @@ namespace MLS.Agent.Tests
                 var document = new HtmlDocument();
                 document.LoadHtml(html);
 
-                document.DocumentNode.SelectSingleNode("//script").InnerHtml.Should().Contain("require.config({paths:{plotly:\'https://cdn.plot.ly/plotly-latest.min\'}});");
+                document.DocumentNode.SelectSingleNode("//script").InnerHtml.Should().Contain("var xplotRequire = require.config({context:'xplot-2.0.0',paths:{plotly:'https://cdn.plot.ly/plotly-1.49.2.min'}});");
             }
 
             [Fact]
@@ -52,7 +52,7 @@ namespace MLS.Agent.Tests
                     .Select(item => item.Trim())
                     .Where(item => !string.IsNullOrWhiteSpace(item))
                     .Should()
-                    .ContainInOrder(@"require(['plotly'], function(Plotly) {",
+                    .ContainInOrder(@"xplotRequire(['plotly'], function(Plotly) {",
                                         "var data = null;",
                                          @"var layout = """";",
                                          $"Plotly.newPlot('{divId}', data, layout);");
--- MLS.Agent.Tests/XplotKernelExtensionTests.cs
+++ MLS.Agent.Tests/XplotKernelExtensionTests.cs
@@ -36,8 +36,8 @@ namespace MLS.Agent.Tests
                 ContainSingle(valueProduced =>
                     valueProduced.FormattedValues.Any(formattedValue =>
                     formattedValue.MimeType == "text/html" &&
-                       formattedValue.Value.ToString().Contains("require([\'plotly\'], function(Plotly)")
-                       && formattedValue.Value.ToString().Contains("require.config({paths:{plotly:\'https://cdn.plot.ly/plotly-latest.min\'}});")
+                       && formattedValue.Value.ToString().Contains("var xplotRequire = require.config({context:'xplot-2.0.0',paths:{plotly:'https://cdn.plot.ly/plotly-1.49.2.min'}});")
+                       formattedValue.Value.ToString().Contains("xplotRequire([\'plotly\'], function(Plotly)")
                  ));
         }
     }
