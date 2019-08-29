3c945faa4765c25a2c80f0322a060a8170350722
Brett V. Forsgren <brettfo@microsoft.com>
2019-08-29 16:53:13 -0700

    add HTML-helper DSL to FSharpKernel (#405)
    * add HTML-helper DSL to FSharpKernel
    
    * rename method and allow nested rendering via IHtmlContent

--- WorkspaceServer/Kernel/FSharpKernelExtensions.cs
+++ WorkspaceServer/Kernel/FSharpKernelExtensions.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Threading.Tasks;
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.FSharp;
 
 namespace WorkspaceServer.Kernel
@@ -10,7 +13,13 @@ namespace WorkspaceServer.Kernel
         public static FSharpKernel UseDefaultRendering(
             this FSharpKernel kernel)
         {
-            // noop while the F# kernel is just a wrapper around fsi.exe forcing all return values to be strings
+            Task.Run(() =>
+                         kernel.SendAsync(
+                         new SubmitCode($@"
+#r ""{typeof(FSharpPocketViewTags).Assembly.Location.Replace("\\", "\\\\")}""
+open {typeof(FSharpPocketViewTags).FullName}
+"))).Wait();
+
             return kernel;
         }
     }
