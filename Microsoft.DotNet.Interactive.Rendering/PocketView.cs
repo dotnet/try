1283b89be0ca8b9ad769c1e7a05abc7322273da6
Brett V. Forsgren <brettfo@microsoft.com>
2019-08-30 12:41:09 -0700

    remove IVT (#417)

--- Microsoft.DotNet.Interactive.Rendering/PocketView.cs
+++ Microsoft.DotNet.Interactive.Rendering/PocketView.cs
@@ -48,7 +48,7 @@ namespace Microsoft.DotNet.Interactive.Rendering
         /// </summary>
         /// <param name="tagName">Name of the tag.</param>
         /// <param name="nested">A nested instance.</param>
-        protected internal PocketView(string tagName, PocketView nested = null) : this(nested)
+        public PocketView(string tagName, PocketView nested = null) : this(nested)
         {
             _tag = tagName.Tag();
         }
@@ -173,7 +173,7 @@ namespace Microsoft.DotNet.Interactive.Rendering
             return true;
         }
 
-        internal void SetContent(object[] args)
+        public void SetContent(object[] args)
         {
             if (args?.Length == 0)
             {
