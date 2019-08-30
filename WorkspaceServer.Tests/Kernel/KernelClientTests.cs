8cfd828eaa962e91ec49797f8d830a46b79eb630
Diego <dicolomb@microsoft.com>
2019-08-30 19:39:06 +0100

    assent as not interactive if not debugging

--- WorkspaceServer.Tests/Kernel/KernelClientTests.cs
+++ WorkspaceServer.Tests/Kernel/KernelClientTests.cs
@@ -2,10 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Assent;
 using FluentAssertions;
@@ -28,8 +28,7 @@ namespace WorkspaceServer.Tests.Kernel
         {
             _configuration = new Configuration()
                 .UsingExtension("json");
-
-            _configuration = _configuration.SetInteractive(true);
+            _configuration = _configuration.SetInteractive(Debugger.IsAttached);
         }
 
         [Fact]
