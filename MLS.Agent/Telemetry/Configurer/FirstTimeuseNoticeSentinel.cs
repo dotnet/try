736016598c5aaa0fd7f4397a2c3832f0b3960926
TIHan <lol.tihan@gmail.com>
2019-09-16 16:01:51 -0700

    Added FirstTimeUseNoticeSentinel

---
+++ MLS.Agent/Telemetry/Configurer/FirstTimeuseNoticeSentinel.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using MLS.Agent.Telemetry.Utils;
+
+namespace MLS.Agent.Telemetry.Configurer
+{
+    // TODO: This isn't fully realized yet. Do we want to have this anyway?
+    internal class FirstTimeUseNoticeSentinel : IFirstTimeUseNoticeSentinel
+    {
+        public static readonly string SENTINEL = $"{Product.Version}.tryFirstUseSentinel";
+
+        public void CreateIfNotExists()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool Exists()
+        {
+            return true;
+        }
+    }
+}
