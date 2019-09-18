4d7b7ef8b5ff9049e4cea536dc9eccdc3ddfb508
Brett V. Forsgren <brettfo@microsoft.com>
2019-09-18 14:26:53 -0700

    add MyGet package upload to build stage on master

---
+++ eng/upload/scripts/UploadPackages.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding(PositionalBinding=$false)]
+param (
+    [string]$apiKey,
+    [string]$feedUrl,
+    [string]$packagesDir
+)
+
+Set-StrictMode -version 2.0
+$ErrorActionPreference = "Stop"
+
+try {
+    $packagePatterns = @(
+        "dotnet-try.*.nupkg",
+        "Microsoft.DotNet.Interactive.*.nupkg", # note that this also matches Microsoft.DotNet.Interactive.Rendering
+        "Microsoft.DotNet.Try.ProjectTemplate.Tutorial.*.nupkg",
+        "MLS.Blazor.*.nupkg",
+        "MLS.WasmCodeRunner.*.nupkg"
+    )
+
+    $errors = 0
+    foreach ($pattern in $packagePatterns) {
+        foreach ($packageName in (Get-Item "$packagesDir\$pattern")) {
+            $fullPath = $packageName.ToString()
+            Write-Host "Uploading package $fullPath."
+            $response = Invoke-WebRequest -Uri $feedUrl -Headers @{"X-NuGet-ApiKey"=$apiKey} -ContentType "multipart/form-data" -InFile "$fullPath" -Method Post -UseBasicParsing
+            if ($response.StatusCode -ne 201) {
+                Write-Host "Failed to upload package.  Status code: $response.StatusCode."
+                $errors++
+            }
+            else {
+                Write-Host "Done."
+            }
+        }
+    }
+
+    exit $errors
+
+}
+catch {
+    Write-Host $_
+    Write-Host $_.Exception
+    Write-Host $_.ScriptStackTrace
+    exit 1
+}
