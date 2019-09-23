f90c9bf584ed908de9012d16b730d2b2866b6b3b
Diego <dicolomb@microsoft.com>
2019-09-23 14:28:52 +0100

    complete tests

--- MLS.Agent.Tests/JupyterKernelSpecTests.cs
+++ MLS.Agent.Tests/JupyterKernelSpecTests.cs
@@ -4,11 +4,9 @@
 using FluentAssertions;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WorkspaceServer.Tests;
-using Xunit;
 
 namespace MLS.Agent.Tests
 {
@@ -21,7 +19,7 @@ namespace MLS.Agent.Tests
             _installedKernels = new List<string>();
         }
 
-        public abstract IJupyterKernelSpec GetJupyterKernelSpec(bool success);
+        public abstract IJupyterKernelSpec GetJupyterKernelSpec(bool success, IReadOnlyCollection<string> error = null);
 
         [FactDependsOnJupyterOnPath]
         public async Task Returns_success_output_when_kernel_installation_succeeded()
@@ -44,7 +42,7 @@ namespace MLS.Agent.Tests
         [FactDependsOnJupyterNotOnPath]
         public async Task Returns_failure_when_kernel_installation_did_not_succeed()
         {
-            var kernelSpec = GetJupyterKernelSpec(false);
+            var kernelSpec = GetJupyterKernelSpec(false, error: new [] { "Could not find jupyter kernelspec module" });
             var kernelDir = Create.EmptyWorkspace().Directory;
 
             var result = await kernelSpec.InstallKernel(kernelDir);
@@ -66,7 +64,7 @@ namespace MLS.Agent.Tests
     public class FileSystemJupyterKernelSpecIntegrationTests : JupyterKernelSpecTests
     {
 
-        public override IJupyterKernelSpec GetJupyterKernelSpec(bool success)
+        public override IJupyterKernelSpec GetJupyterKernelSpec(bool success, IReadOnlyCollection<string> error = null)
         {
             return new FileSystemJupyterKernelSpec();
         }
@@ -74,9 +72,9 @@ namespace MLS.Agent.Tests
 
     public class InMemoryJupyterKernelSpecTests : JupyterKernelSpecTests
     {
-        public override IJupyterKernelSpec GetJupyterKernelSpec(bool success)
+        public override IJupyterKernelSpec GetJupyterKernelSpec(bool success, IReadOnlyCollection<string> error = null)
         {
-            return new InMemoryJupyterKernelSpec(success);
+            return new InMemoryJupyterKernelSpec(success, error);
         }
     }
 }
