63ae1f1758efeaa71776649decc364758e8f227a
Diego <dicolomb@microsoft.com>
2019-11-27 21:00:30 +0000

    unquote the quote

--- eng/build.sh
+++ eng/build.sh
@@ -36,7 +36,7 @@ function TestUsingNPM() {
 }
 
 # invoke regular build/test script
-. "$scriptroot/common/build.sh" "-projects $scriptroot/../dotnet-interactive.sln;$scriptroot/../dotnet-try.sln" $args
+. "$scriptroot/common/build.sh" -projects "$scriptroot/../dotnet-interactive.sln;$scriptroot/../dotnet-try.sln" $args
 
 # directly invoke npm tests
 if [[ "$run_tests" == "true" ]]; then
