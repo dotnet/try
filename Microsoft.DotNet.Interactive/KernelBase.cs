d2a96eede5d684750eccbc27330fb52aa0def28c
Diego <dicolomb@microsoft.com>
2020-01-22 21:51:18 +0000

    remove null check

--- Microsoft.DotNet.Interactive/KernelBase.cs
+++ Microsoft.DotNet.Interactive/KernelBase.cs
@@ -105,7 +105,7 @@ namespace Microsoft.DotNet.Interactive
                     }
                     else
                     {
-                        await (handlingKernel ?? context.HandlingKernel).SendAsync(command);
+                        await handlingKernel.SendAsync(command);
                     }
                 }
             }
