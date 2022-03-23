e1e751519e4f1483c4e9d73a7c686d4a27731f82
Diego Colombo <dicolomb@microsoft.com>
2022-03-23 19:42:19 +0000

    add playwright

---
+++ src/Microsoft.TryDotNet.Tests/playwright.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Xunit;
+
+namespace Microsoft.TryDotNet.Tests;
+
+public class playwright 
+{
+    public playwright()
+    {
+        var exitCode = Playwright.Program.Main(new[] { "install" });
+        if (exitCode != 0)
+        {
+            throw new Exception($"Playwright exited with code {exitCode}");
+        }
+    }
+
+    [Fact]
+    public async Task Works()
+    {
+        using var playwright = await Playwright.Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://playwright.dev/dotnet");
+        await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+
+        
+    }
+    
+}
