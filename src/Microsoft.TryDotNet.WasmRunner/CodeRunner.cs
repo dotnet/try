70742dba21541a9cb466e54c48fae0e2d9fe9158
Diego Colombo <dicolomb@microsoft.com>
2022-03-31 16:22:45 +0100

    implement interop

--- src/Microsoft.TryDotNet.WasmRunner/CodeRunner.cs
+++ src/Microsoft.TryDotNet.WasmRunner/CodeRunner.cs
@@ -1,4 +1,7 @@
-﻿using System.Reactive.Disposables;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Reactive.Disposables;
 using System.Reflection;
 
 namespace Microsoft.TryDotNet.WasmRunner;
---
+++ src/Microsoft.TryDotNet.WasmRunner/CodeRunnerAdapter.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.JSInterop;
+
+namespace Microsoft.TryDotNet.WasmRunner;
+
+internal class CodeRunnerAdapter
+{
+    private readonly CodeRunner _runner;
+    private readonly IJSRuntime _jsInterop;
+
+    public CodeRunnerAdapter(CodeRunner runner, IJSRuntime jsInterop)
+    {
+        _runner = runner;
+        _jsInterop = jsInterop;
+    }
+
+    [JSInvokable]
+    public async Task<SerializableCodeRunnerResult> RunAssembly(string base64EncodedAssembly)
+    {
+        var result = await _runner.RunAssemblyEntryPoint(base64EncodedAssembly,
+            output => _jsInterop.InvokeAsync<object>("publishCodeRunnerStdOut", output),
+            error => _jsInterop.InvokeAsync<object>("publishCodeRunnerStdError", error)
+            );
+
+        return new SerializableCodeRunnerResult(result.Succeeded, result.Exception?.Message, result.RunnerException?.Message);
+    }
+}
+
+internal record SerializableCodeRunnerResult(bool Success, string? Error, string? RunnerError);
