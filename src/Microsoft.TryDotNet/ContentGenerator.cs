b02bafbac563bb034b4a135a03a9ae945b0ceeae
Diego Colombo <dicolomb@microsoft.com>
2022-04-01 12:24:11 +0100

    getting started with editor page generator

---
+++ src/Microsoft.TryDotNet/ContentGenerator.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Html;
+
+namespace Microsoft.TryDotNet
+{
+    public class ContentGenerator
+    {
+        public static async Task<IHtmlContent> GenerateEditorPage(HttpRequest request)
+        {
+            // use pocket view
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
