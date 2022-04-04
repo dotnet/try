6d2379688f520f511ce959565dacb6fbf3e3903b
Brett V. Forsgren <brettfo@microsoft.com>
2022-04-04 17:16:36 -0600

    remove xterm functionality

--- src/microsoft-trydotnet/test/domInjection.specs.ts
+++ src/microsoft-trydotnet/test/domInjection.specs.ts
@@ -4,11 +4,11 @@
 import * as chai from "chai";
 import { tryDotNetModes, createRunOutputElements, PreOutputPanel } from "../src/index";
 import { JSDOM } from "jsdom";
-import { XtermTerminal } from "../src/XtermTerminal";
 
 chai.should();
 
 describe("dom utilities", () => {
+    // TODO: move this to integration tests
     describe("can create output panels from a div", () => {
 
         it("even when not specified the type", () => {
@@ -33,54 +33,6 @@ describe("dom utilities", () => {
 
             outputPanel.should.not.be.null;
             outputPanel.constructor.name.should.be.equal(PreOutputPanel.name);
-        });
-
-        it("as standard panel", () => {
-            let configuration = {
-                hostOrigin: "https://docs.microsoft.com"
-            };
-            let dom = new JSDOM(
-                `<!DOCTYPE html>
-            <html lang="en">
-            <body>
-                <div data-trydotnet-mode="runResult" data-trydotnet-session-id="a" data-trydotnet-output-type="standard"></div>
-            </body>
-            </html>`,
-                {
-                    url: configuration.hostOrigin,
-                    runScripts: "dangerously"
-                });
-
-            let doc = dom.window.document;
-            let outputPanelContainer = doc.querySelector<HTMLDivElement>(`div[data-trydotnet-mode=${tryDotNetModes[tryDotNetModes.runResult]}]`);
-            let { outputPanel } = createRunOutputElements(outputPanelContainer, doc);
-
-            outputPanel.should.not.be.null;
-            outputPanel.constructor.name.should.be.equal(PreOutputPanel.name);
-        });
-
-        it("as terminal", () => {
-            let configuration = {
-                hostOrigin: "https://docs.microsoft.com"
-            };
-            let dom = new JSDOM(
-                `<!DOCTYPE html>
-            <html lang="en">
-            <body>
-                <div data-trydotnet-mode="runResult" data-trydotnet-session-id="a" data-trydotnet-output-type="terminal"></div>
-            </body>
-            </html>`,
-                {
-                    url: configuration.hostOrigin,
-                    runScripts: "dangerously"
-                });
-
-            let doc = dom.window.document;
-            let outputPanelContainer = doc.querySelector<HTMLDivElement>(`div[data-trydotnet-mode=${tryDotNetModes[tryDotNetModes.runResult]}]`);
-            let { outputPanel } = createRunOutputElements(outputPanelContainer, doc);
-
-            outputPanel.should.not.be.null;
-            outputPanel.constructor.name.should.be.equal(XtermTerminal.name);
         });
     });
 });
