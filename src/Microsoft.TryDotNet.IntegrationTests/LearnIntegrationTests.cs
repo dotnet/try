7c420d83e033440ea7b3f05d09673df601bd61d9
Diego Colombo <dicolomb@microsoft.com>
2022-06-09 16:36:49 +0100

    enable tests

--- src/Microsoft.TryDotNet.IntegrationTests/LearnIntegrationTests.cs
+++ src/Microsoft.TryDotNet.IntegrationTests/LearnIntegrationTests.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Playwright;
 using Xunit;
@@ -23,6 +24,7 @@ public class LearnIntegrationTests : PlaywrightTestBase, IClassFixture<LearnFixt
     public async Task loads_trydotnet()
     {
         var page = await Playwright.Browser!.NewPageAsync();
+        
         var learnRoot = Learn.Url!;
         var trydotnetOrigin = TryDotNet.Url!;
         var trydotnetUrl = new Uri(trydotnetOrigin, "api/trydotnet.min.js");
@@ -36,12 +38,78 @@ public class LearnIntegrationTests : PlaywrightTestBase, IClassFixture<LearnFixt
         var pageUri = new Uri(QueryHelpers.AddQueryString(new Uri(learnRoot,"DocsHost.html").ToString(), param!));
         await page.GotoAsync(pageUri.ToString());
         await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+        
+        await page.FindEditor();
+    }
+
+    [Fact]
+    public async Task can_load_code()
+    {
+        var page = await Playwright.Browser!.NewPageAsync();
+        var interceptor = new MessageInterceptor();
+        await interceptor.InstallAsync(page);
+
+        var learnRoot = Learn.Url!;
+        var trydotnetOrigin = TryDotNet.Url!;
+        var trydotnetUrl = new Uri(trydotnetOrigin, "api/trydotnet.min.js");
+
+        var param = new Dictionary<string, string>
+        {
+            ["trydotnetUrl"] = trydotnetUrl.ToString(),
+            ["trydotnetOrigin"] = trydotnetOrigin.ToString(),
+        };
+
+        var pageUri = new Uri(QueryHelpers.AddQueryString(new Uri(learnRoot, "DocsHost.html").ToString(), param!));
+        await page.GotoAsync(pageUri.ToString());
+        await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
 
         var dotnetOnline = new DotNetOnline(page);
 
         await dotnetOnline.FocusAsync();
+        var documentOpenAwaiter = interceptor.AwaitForMessage("DocumentOpened");
         await dotnetOnline.SetCodeAsync("Console.WriteLine(123);");
+        await documentOpenAwaiter;
+       
+        await page.TestScreenShotAsync();
+        var text = await page.FrameLocator("body > div > div.dotnet-online-editor-section > iframe").GetEditorContentAsync();
+        text.Should().Contain("Console.WriteLine(123);");
+    }
+
+    [Fact]
+    public async Task outputs_are_rendered()
+    {
+        var page = await Playwright.Browser!.NewPageAsync();
+        var interceptor = new MessageInterceptor();
+        await interceptor.InstallAsync(page);
+
+        var learnRoot = Learn.Url!;
+        var trydotnetOrigin = TryDotNet.Url!;
+        var trydotnetUrl = new Uri(trydotnetOrigin, "api/trydotnet.min.js");
+
+        var param = new Dictionary<string, string>
+        {
+            ["trydotnetUrl"] = trydotnetUrl.ToString(),
+            ["trydotnetOrigin"] = trydotnetOrigin.ToString(),
+        };
+
+        var pageUri = new Uri(QueryHelpers.AddQueryString(new Uri(learnRoot, "DocsHost.html").ToString(), param!));
+        await page.GotoAsync(pageUri.ToString());
+        await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+        var dotnetOnline = new DotNetOnline(page);
+
+        await dotnetOnline.FocusAsync();
+        var documentOpenAwaiter = interceptor.AwaitForMessage("DocumentOpened");
+        await dotnetOnline.SetCodeAsync("Console.WriteLine(123);");
+        await documentOpenAwaiter;
+        await Task.Delay(1000);
+        var run = interceptor.AwaitForMessage("RunCompleted");
         await dotnetOnline.ExecuteAsync();
-        throw new NotImplementedException();
+        await run;
+        
+        await page.TestScreenShotAsync();
+        var outputElement = page.Locator("body > div > div.dotnet-online-editor-section > pre");
+        var result = await outputElement.TextContentAsync();
+        result.Should().Be("123\n");
     }
 }
