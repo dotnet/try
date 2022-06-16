c6a8bfc7ac7a578f204bca70ec26a32c0c34e1df
Diego Colombo <dicolomb@microsoft.com>
2022-06-16 23:51:56 +0100

    use console interception

--- src/Microsoft.TryDotNet.IntegrationTests/TryDotNetJsIntegrationTests.cs
+++ src/Microsoft.TryDotNet.IntegrationTests/TryDotNetJsIntegrationTests.cs
@@ -67,10 +67,19 @@ public class TryDotNetJsIntegrationTests : PlaywrightTestBase, IClassFixture<Lea
         var dotnetOnline = new DotNetOnline(page);
 
         await dotnetOnline.FocusAsync();
-        var documentOpenAwaiter = interceptor.AwaitForMessage("DocumentOpened");
-        await dotnetOnline.SetCodeAsync("Console.WriteLine(123);");
-        await documentOpenAwaiter;
-       
+        await page.RunAndWaitForConsoleMessageAsync(async () =>
+            {
+                var documentOpenAwaiter = interceptor.AwaitForMessage("DocumentOpened");
+                await dotnetOnline.SetCodeAsync("Console.WriteLine(123);");
+                await documentOpenAwaiter;
+            },
+            new PageRunAndWaitForConsoleMessageOptions
+            {
+                Predicate = message => message.Text.Contains("[trydotnet-editor] [MonacoEditorArapter.setCode]: Console.WriteLine(123);"),
+                Timeout = Debugger.IsAttached ? 0.0f : (float)TimeSpan.FromMinutes(10).TotalMilliseconds
+            }
+        );
+
         await page.TestScreenShotAsync();
         var text = await page.FrameLocator("body > div > div.dotnet-online-editor-section > iframe").GetEditorContentAsync();
         text.Should().Contain("Console.WriteLine(123);");
