0e551bf17c4543ee64a3bca605fded0ccfe40452
Brett V. Forsgren <brettfo@microsoft.com>
2022-08-31 11:45:43 -0600

    mark and skip integration tests

---
+++ src/Microsoft.TryDotNet.IntegrationTests/IntegrationTestFactAttribute.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Xunit;
+
+namespace Microsoft.TryDotNet.IntegrationTests
+{
+    internal class IntegrationTestFactAttribute : FactAttribute
+    {
+        private const string EnvironmentVariableName = "RunIntegrationTests";
+
+        public IntegrationTestFactAttribute()
+        {
+            var variableValue = Environment.GetEnvironmentVariable(EnvironmentVariableName) ?? "false";
+            switch (variableValue.ToLowerInvariant())
+            {
+                case "1":
+                case "true":
+                    break;
+                default:
+                    Skip = $"Skipping integration tests because environment variable '{EnvironmentVariableName}' was not 'true' or '1'.";
+                    break;
+            }
+        }
+    }
+}
