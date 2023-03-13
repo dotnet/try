8daf3bd77f61f068fcc07c0c76d94875a977eac3
Diego Colombo <colombod@me.com>
2023-03-13 11:34:46 +0000

    remove comments

--- src/Microsoft.TryDotNet/Program.cs
+++ src/Microsoft.TryDotNet/Program.cs
@@ -75,17 +75,12 @@ public class Program
                 var kernelEvents = new List<KernelEvent>();
                 await using (var requestBody = request.Body)
                 {
-                    //Debugger.Launch();
                     using var kernel = new CSharpProjectKernel("project-kernel");
                     var body = await new StreamReader(requestBody).ReadToEndAsync();
 
                     var bundle = JsonDocument.Parse(body).RootElement;
 
                     var commandEnvelopes = ReadCommands(bundle).ToList();
-                    //if (commandEnvelopes.FirstOrDefault(ce => ce.Command is OpenProject) is not null)
-                    //{
-                    //    Debugger.Launch();
-                    //}
 
                     foreach (var commandEnvelope in commandEnvelopes)
                     {
