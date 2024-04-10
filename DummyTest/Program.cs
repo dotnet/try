c691af61e54ddf394fbbb4721dfa1e185a93e3c1
Osvaldo Calles <oscalles@microsoft.com>
2024-04-09 20:47:08 -0700

    Read environment variable POCKETLOGGER_LOG_PATH

--- DummyTest/Program.cs
+++ DummyTest/Program.cs
@@ -1,2 +1,2 @@
-﻿string logPath = Environment.GetEnvironmentVariable("POCKETLOGGER_LOG_PATH");
+﻿string? logPath = Environment.GetEnvironmentVariable("POCKETLOGGER_LOG_PATH");
 Console.WriteLine($"POCKETLOGGER_LOG_PATH: {logPath}");
